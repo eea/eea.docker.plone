acf926cca4c1ea331a0e716b295b3a0751de4383
Alin Voinea <contact@avoinea.com>
2019-12-16 21:05:58 +0200

    Relese v6

--- src/docker/common-entrypoint.sh
+++ src/docker/common-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Fixing permissions for external /data volumes"
-mkdir -p /data/log /plone/instance/src
-find /data/log -not -user plone -exec chown plone:plone {} \+
-find /plone/instance/src -not -user plone -exec chown plone:plone {} \+
+mkdir -p /data/blobstorage /data/cache /data/filestorage /data/instance /data/ /data/log /data/zeoserver
+mkdir -p /plone/instance/src
+find /data  -not -user plone -exec chown plone:plone {} \+
+find /plone -not -user plone -exec chown plone:plone {} \+
 
 exec /plone-entrypoint.sh "$@"
