6e1f40e7d1c6df2ee30d63c45aad505fea16eec8
valentinab25 <valentinab25>
2020-02-27 15:51:42 +0200

    Prepare release on manual tag creation

--- src/docker/calculate_next_release.sh
+++ src/docker/calculate_next_release.sh
@@ -1,1 +1,1 @@
-major=$(grep FROM Dockerfile | awk -F':|-' '{print $2}');let minor=$(git tag | grep -cE $major-[0-9]+)+1; echo $major-$minor
+major=$(grep FROM Dockerfile | awk -F':|-' '{print $2}');let minor=$(git describe --tags --abbrev=0 | grep -E $major-[0-9]+ | cut -d- -f2 )+1; echo $major-$minor
