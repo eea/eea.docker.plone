d7fc3ad4a3bf6f409ae1e1e2d2fb15169b3c22e1
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2023-04-28 13:16:12 +0300

    chore: fix next version release number

--- src/docker/calculate_next_release.sh
+++ src/docker/calculate_next_release.sh
@@ -1,1 +1,1 @@
-major=$(grep FROM Dockerfile | awk -F':|-' '{print $2}');let minor=$(git describe --tags --abbrev=0 | grep -E $major-[0-9]+ | cut -d- -f2 )+1; echo $major-$minor
+major=$(grep FROM Dockerfile | awk -F':|-' '{print $2}');let minor=$(git tag | grep -E $major-[0-9]+ | sort -V | tail -n 1 | cut -d- -f2 )+1; echo $major-$minor
